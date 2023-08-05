92523554dfc5c34830f5161c7bdc0134e527ab3e
CoderSerio <CoderSerio@2779066456@qq.com>
2023-08-05 15:58:45 +0800

    feat: 静态资源解析

--- src/core/server.ts
+++ src/core/server.ts
@@ -1,18 +1,40 @@
 import * as http from "http";
 import * as childProcess from "child_process";
-// import { resolve } from "path";
+import * as path from "path";
+import * as fs from "fs";
+
+const extName2FileType: Record<string, string> = {
+  ".png": "image/png",
+  ".jpg": "image/jpg",
+  ".svg": "image/svg+xml;charset=utf-8",
+  ".html": "text/html;charset=utf8",
+  ".js": "application/javascript;charset=utf8",
+  ".css": "text/css;charset=utf8",
+};
 
 // TODO: 传入端口等参数
 export const startServer = async () => {
   const port = 2333;
   const server = http.createServer((req, res) => {
-    console.log(req);
-    console.log("dir", __dirname);
-    // const htmlFilePath = resolve(, '')
-    res.end("Hello");
+    console.log("路径", req.url);
+    let resFilePath = "/index.html";
+    if (req.url !== "/") {
+      resFilePath = req?.url || "/index.html";
+    }
+    const extName = path.extname(resFilePath);
+    const fileType = extName2FileType[extName] ?? extName2FileType[".html"];
+
+    res.setHeader("content-type", fileType);
+    res.on("close", () => {
+      console.log("success!");
+    });
+
+    const htmlFilePath = path.resolve(__dirname, `../view${resFilePath}`);
+    fs.createReadStream(htmlFilePath).pipe(res);
   });
 
   server.listen(port, () => {
+    console.log(__dirname);
     console.log(`服务器已经在${port}上开启`);
     childProcess.exec(`start http://localhost:${port}`);
   });
